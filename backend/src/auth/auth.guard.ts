38c325010ceb685b1db6ae4414d9c955197ec30d
ArthurLuis <alfa@cin.ufpe.br>
2025-03-15 02:41:18 -0300

    feat: auth created

---
+++ backend/src/auth/auth.guard.ts
@@ -0,0 +1,46 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+interface JwtPayload {
+  sub: string;
+  email?: string;
+}
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) throw new UnauthorizedException('Token is required');
+
+    try {
+      const payload: JwtPayload = await this.jwtService.verifyAsync<JwtPayload>(
+        token,
+        {
+          secret: process.env.SECRET_KEY,
+        },
+      );
+      request['sub'] = payload;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const authorization = request.headers.authorization;
+    if (!authorization) return undefined;
+
+    const [type, token] = authorization.split(' ');
+    return type === 'Bearer' ? token : undefined;
+  }
+}
