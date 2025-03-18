531e2f2d63c8b0c06edf59dd02621dd04517f422
ArthurLuis <alfa@cin.ufpe.br>
2025-03-18 14:49:54 -0300

    feat: loading at chat

--- frontend/app/components/Chat/Chat.tsx
+++ frontend/app/components/Chat/Chat.tsx
@@ -1,12 +1,18 @@
-'use client';import React, {useState} from 'react';
+'use client';
+import React, {useState, useEffect, useRef} from 'react';
 import Balloon from '../Balloon/Baloon';
 import AppInput from '../AppInput/AppInput';
 import AppButton from '../AppButton/AppButton';
-import Image from 'next/image';
+import axios from 'axios';
+import Loading from '../Loading/Loading';
+import DownloadButton from '../DownloadButton/DownloadButton'; // Importando o DownloadButton
+import ImageModal from '../ImageModal/ImageModal';
 
 interface ChatProps {
   aiResponse: string;
   photo: string;
+  documentId: string;
+  extractedText: string;
 }
 
 interface Interaction {
@@ -14,61 +20,113 @@ interface Interaction {
   llmAnswer: string;
 }
 
-const Chat = ({aiResponse, photo}: ChatProps) => {
+const getTokenFromCookies = (): string | null => {
+  const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+    const [name, value] = cookie.split('=');
+    acc[name] = value;
+    return acc;
+  }, {} as {[key: string]: string});
+
+  return cookies.authToken || null; 
+};
+
+const Chat = ({aiResponse, photo, documentId, extractedText}: ChatProps) => {
   const [interactions, setInteractions] = useState<Interaction[]>([]);
   const [userInput, setUserInput] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
   const handleSendMessage = async () => {
-    if (userInput.trim() === '') return; // Não enviar se estiver vazio
+    if (userInput.trim() === '') return; 
 
-    // Adiciona a mensagem do usuário
-    const newInteraction: Interaction = {
-      userQuery: userInput,
-      llmAnswer: 'resposta do backend',
-    };
-    // Atualiza o estado com as novas mensagens
-    setInteractions((prevInteractions) => [
-      ...prevInteractions,
-      newInteraction,
-    ]);
+    const authToken = getTokenFromCookies();
+    if (!authToken) {
+      console.error('Token JWT não encontrado nos cookies.');
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/document/${documentId}`,
+        {question: userInput},
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      setInteractions((prev) => [
+        ...prev,
+        {userQuery: userInput, llmAnswer: response.data.response},
+      ]);
+    } catch (error) {
+      console.error('Erro ao enviar mensagem:', error);
+      setInteractions((prev) => [
+        ...prev,
+        {userQuery: userInput, llmAnswer: 'Erro ao obter resposta.'},
+      ]);
+    } finally {
+      setLoading(false);
+      setUserInput('');
+    }
   };
-  return (
-    <>
-      <div className='flex flex-col justify-between relative bg-white rounded-lg shadow-md pr-20 pt-14 pl-10 max-w-[1200px] mx-auto mt-20 h-[calc(100vh-200px)]  overflow-y-auto'>
-        <div>
-          <Balloon type='user'>
-            <Image
-              src={photo}
-              alt='Imagem enviada'
-              className='max-w-[150px] rounded-lg'
-            ></Image>
-          </Balloon>
-          <Balloon type='app'>{aiResponse}</Balloon>
-          {interactions.map((interaction, index) => (
-            <div key={index}>
-              <Balloon type='user'>{interaction.userQuery}</Balloon>
-              <Balloon type='app'>{interaction.llmAnswer}</Balloon>
-            </div>
-          ))}
-        </div>
 
-        {/* Campo de entrada e botão de enviar fixados na parte inferior */}
-        <div className='flex sticky bottom-0 left-0 right-0 bg-white p-4 justify-center gap-4'>
-          <AppInput
-            type='text'
-            placeholder='Faça uma pergunta...'
-            width={500}
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-          />
-          <AppButton
-            title='Enviar'
-            bgColor='bg-gray-800'
-            onClick={handleSendMessage}
-          />
-        </div>
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({behavior: 'smooth'});
+    }
+  }, [interactions]);
+
+  return (
+    <div className='flex flex-col justify-between relative bg-white rounded-lg shadow-md pr-20 pt-14 pl-10 max-w-[1200px] mx-auto mt-20 h-[calc(100vh-200px)] overflow-y-auto'>
+      <div>
+        <Balloon type='user'>
+          <ImageModal src={photo} alt='Documento' width='150px' height='150px'></ImageModal>
+        </Balloon>
+        <Balloon type='app'>{extractedText}</Balloon>
+        <Balloon type='app'>{aiResponse}</Balloon>
+        {interactions.map((interaction, index) => (
+          <div key={index}>
+            <Balloon type='user'>{interaction.userQuery}</Balloon>
+
+            <Balloon type='app'>  
+              {interaction.llmAnswer === '...' ? (
+                <Loading isLoading />
+              ) : (
+                interaction.llmAnswer
+              )}
+            </Balloon>
+          </div>
+        ))}
+        <div ref={messagesEndRef} />
       </div>
-    </>
+
+      <div className='absolute left-4 top-4'>
+        <DownloadButton documentId={documentId} documentTitle='Documento' />
+      </div>
+
+      <div className='flex sticky bottom-0 left-0 right-0 bg-white p-4 justify-center gap-4'>
+        <AppInput
+          type='text'
+          placeholder='Faça uma pergunta...'
+          width={500}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          disabled={loading}
+        />
+        <AppButton
+          title={loading ? 'Enviando...' : 'Enviar'}
+          bgColor='bg-gray-800'
+          onClick={handleSendMessage}
+          disabled={loading}
+        />
+      </div>
+    </div>
   );
 };
 
