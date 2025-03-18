dd6633cc89cdc532b3612a1199b29e6b940b3892
ArthurLuis <alfa@cin.ufpe.br>
2025-03-18 14:50:26 -0300

    feat: new loading component

---
+++ frontend/app/components/Loading/Loading.tsx
@@ -0,0 +1,36 @@
+'use client';import React from 'react';
+import Lottie from 'lottie-react';
+import loadingAnimation from '@/public/loading.json';
+
+interface LoadingProps {
+  isLoading: boolean;
+  progress?: number;
+  showProgress?: boolean;
+}
+
+const Loading = ({
+  isLoading,
+  progress = 0,
+  showProgress = false,
+}: LoadingProps) => {
+  return (
+    isLoading && (
+      <div className='flex flex-col items-center justify-center'>
+        <div className='w-48 h-48 md:w-64 md:h-64'>
+          <Lottie animationData={loadingAnimation} loop autoPlay />
+        </div>
+
+        {showProgress && (
+          <div className='w-32 h-1 bg-gray-300 mt-2 rounded-full overflow-hidden'>
+            <div
+              className='h-full bg-black transition-all'
+              style={{width: `${progress}%`}}
+            />
+          </div>
+        )}
+      </div>
+    )
+  );
+};
+
+export default Loading;
