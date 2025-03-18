06e5ce6cd3128ce0adf4576835c2d5cee1d2424f
ArthurLuis <alfa@cin.ufpe.br>
2025-03-18 14:49:28 -0300

    feat: animation on ballon

--- frontend/app/components/Balloon/Baloon.tsx
+++ frontend/app/components/Balloon/Baloon.tsx
@@ -1,5 +1,7 @@
 'use client';
 import React from 'react';
+import {motion} from 'framer-motion';
+import ReactMarkdown from 'react-markdown';
 
 interface BalloonProps {
   children: React.ReactNode;
@@ -7,20 +9,31 @@ interface BalloonProps {
 }
 
 const Balloon = ({children, type}: BalloonProps) => {
+  const content = typeof children === 'string' ? children : '';
+
   return (
-    <div
+    <motion.div
+      initial={{scale: 0.7, opacity: 0}}
+      animate={{scale: 1, opacity: 1}}
+      transition={{
+        type: 'spring',
+        stiffness: 100,
+        damping: 10,
+        delay: type === 'app' ? 0.5 : 0, 
+      }}
       className={`flex ${type === 'user' ? 'justify-end' : 'justify-start'}`}
     >
       <div
-        className={`rounded-lg shadow-md p-4 max-w-[80%] ${
+        onClick={(e) => e.stopPropagation()} 
+        className={`rounded-lg shadow-md p-4 max-w-[80%] mb-6 ${
           type === 'user'
             ? 'bg-[#1E1E1E] text-white'
             : 'bg-[#F5F5F5] text-black'
         }`}
       >
-        {children}
+        {content ? <ReactMarkdown>{content}</ReactMarkdown> : children}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
